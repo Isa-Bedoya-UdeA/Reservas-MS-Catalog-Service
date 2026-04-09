06a16bb43110e4a85690e3d4fbd67add6c32da5f
CodeInTheIzzyverse <dev.izzyverse@gmail.com>
2026-04-09 01:58:02 -0500

    Updated Conecctions

--- src/main/java/com/codefactory/reservasmscatalogservice/client/AuthClient.java
+++ src/main/java/com/codefactory/reservasmscatalogservice/client/AuthClient.java
@@ -1,4 +1,23 @@
 package com.codefactory.reservasmscatalogservice.client;
 
-public class AuthClient {
+import com.codefactory.reservasmscatalogservice.config.FeignConfig;
+import com.codefactory.reservasmscatalogservice.dto.external.ExternalClientDTO;
+import com.codefactory.reservasmscatalogservice.dto.external.ExternalProviderDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(
+        name = "auth-service",
+        url = "${services.auth-service.url}",
+        configuration = FeignConfig.class
+)
+public interface AuthClient {
+
+    @GetMapping("/api/users/clients/{id}")
+    ResponseEntity<ExternalClientDTO> getClientById(@PathVariable("id") Long id);
+
+    @GetMapping("/api/users/providers/{id}")
+    ResponseEntity<ExternalProviderDTO> getProviderById(@PathVariable("id") Long id);
 }
